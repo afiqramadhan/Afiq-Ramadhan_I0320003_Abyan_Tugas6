4ebe718d176a0626726777afc9b7da36f1528e26
Afiq R <ramadhanendraputra@gmail.com>
2021-04-07 00:14:11 +0700

    pengerjaan soal 1

---
+++ I0320003_Soal 2_Tugas 6.py
@@ -0,0 +1,9 @@
+print('Program Menghitung Rata-Rata')
+print('============================')
+# input data
+x = float(input('masukkan nilai yang anda akan hitung: '))
+while x != ():
+    y = float(input('masukkan nilai yang anda akan hitung: '))
+    print(x,y)
+else:
+    print('program selesai')
