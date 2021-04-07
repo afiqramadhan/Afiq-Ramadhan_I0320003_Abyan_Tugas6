e02325037b0f08faff99fa14212ef2f7bd4b95a7
Afiq R <ramadhanendraputra@gmail.com>
2021-04-07 11:40:01 +0700

    pengerjan soal

--- I0320003_Soal 1_Tugas6.py
+++ I0320003_Soal 1_Tugas6.py
@@ -3,14 +3,3 @@ while i <= 10:
     print('Baris ke-%d: Hello World!' %i)
     i = i+1
 
-print('Program Menghitung Rata-Rata')
-print('============================')
-# input data
-data = []
-total = 0
-n = int(input('masukkan banyaknya elemen data: '))
-for x in range(n):
-    nilai = float(input('masukkan nilai ke-{} : '.format(x+1)))
-    data.append(nilai)
-print('\n hasil nilai total adalah : {} '.format(sum(data)))
-print('\n hasil rata-rata adalah : {} '.format(sum(data)/n))
